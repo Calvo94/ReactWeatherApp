79529338f2cebb881efbbc7e8022fa76b5c85eed
Said Mançouri Azzouzi <Said Manouri Azzouzi>
2017-05-01 21:28:09 +0100

    prep repo for Heroku

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ var app = express();
 const PORT = process.env.port || 3000;
 
 app.use(function(req,res,next){
-  if(req.protocol === 'http'){
-    next();
+  if(req.headers['x-forwarded-proto']!='https')
+
+    res.redirect('http://' + req.hostname + req.url);
   }
   else{
-    res.redirect('https://' + req.hostname + req.url);
+      next();
   }
+
 });
 
 app.use(express.static('public'));

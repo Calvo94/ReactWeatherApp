cce24fb7f542f96e22ec26aa920b2fdb1dc09421
Said Mançouri Azzouzi <Said Manouri Azzouzi>
2017-05-02 23:22:23 +0100

    Update middleware for localhost

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(function(req,res,next){
   if(req.headers['x-forwarded-proto']==='https'){
-    console.log('https://' +  req.get('Host') + req.url);
-    res.redirect('https://' +  req.get('Host') + req.url);
+    console.log('http://' +  req.get('Host') + req.url);
+    res.redirect('http://' +  req.get('Host') + req.url);
   }
   else{
     next();

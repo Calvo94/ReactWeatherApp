383fd8e8fdbb911b1b485f73c1ee42836dae6106
Said Mançouri Azzouzi <Said Manouri Azzouzi>
2017-05-04 15:17:38 +0100

    style about page & link to ressources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,21 @@ var React = require('react');
 
 var About = (props) =>{
   return (
-    <h3>About component</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application builde on React. I have built this for the complete react web app developer course.</p>
+      <p>
+        Here are some of the tools I used :
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the Javascript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
+    </div>
   )
 }
 
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -8,7 +8,7 @@ var Main = (props)=>{
       <div>
         <Nav/>
         <div className="row">
-          <div class="columns medium-6 large-4 small-centered">
+          <div className="columns medium-6 large-4 small-centered">
             {props.children}
           </div>
         </div>

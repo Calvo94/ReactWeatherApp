ec88bd697bd9582156ef0c54b9b1b0bb5eebd974
Said Mançouri Azzouzi <Said Manouri Azzouzi>
2017-05-04 13:05:29 +0100

    Add foundation & jQuery

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,49 @@
-module.exports= {
-  entry: './app/app.jsx',
+var webpack = require('webpack');
+
+module.exports = {
+  entry: ['script!jquery/dist/jquery.min.js',
+  'script!foundation-sites/dist/foundation.min.js',
+  './app/app.jsx',
+],
+externals: {
+  jquery:'jQuery'
+},
+plugins: [
+  new webpack.ProvidePlugin({
+    '$':'jquery',
+    'jQuery':'jquery'
+  })
+],
+
   output: {
     path: __dirname,
     filename: './public/bundle.js'
   },
-  resolve:{
+  resolve: {
     root: __dirname,
     alias: {
       Main: 'app/components/Main.jsx',
-      Nav : 'app/components/Nav.jsx',
+      Nav: 'app/components/Nav.jsx',
       Weather: 'app/components/Weather.jsx',
+      WeatherForm: 'app/components/WeatherForm.jsx',
+      WeatherMessage: 'app/components/WeatherMessage.jsx',
       About: 'app/components/About.jsx',
       Examples: 'app/components/Examples.jsx',
-      WeatherForm: 'app/components/WeatherForm.jsx',
-      WeatherMessage:'app/components/WeatherMessage.jsx',
       openWeatherMap: 'app/api/openWeatherMap.jsx'
     },
-    extensions: ['','.js','.jsx']
+    extensions: ['', '.js', '.jsx']
   },
-  module:{
-    loaders:[
+  module: {
+    loaders: [
       {
         loader: 'babel-loader',
-        query:{
-          presets: ['react','es2015','stage-0']
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
         },
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/
       }
     ]
   },
-  devtool:'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map'
 };
